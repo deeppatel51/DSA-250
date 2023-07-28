1907e2273b18302080bdd3047634dfe7fb110fb4
Deep Patel <88723958+deeppatel51@users.noreply.github.com>
2023-07-28 20:08:43 +0530

    Create Recursion_FloodFill.cpp

---
+++ Recursion_FloodFill.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& mat,int i,int j,int color,int r,int c,int cellColor){
+
+        if(i<0 || i>=r || j<0 || j>=c || mat[i][j] != cellColor){
+            return;
+        }
+    
+        mat[i][j] = color;
+        
+        dfs(mat,i-1,j,color,r,c,cellColor);
+        dfs(mat,i+1,j,color,r,c,cellColor);
+        dfs(mat,i,j+1,color,r,c,cellColor);
+        dfs(mat,i,j-1,color,r,c,cellColor);
+    }
+    
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& mat, int sr, int sc, int color) {
+        int r=mat.size();
+        int c=mat[0].size();
+        
+        int cellColor = mat[sr][sc];
+        if(cellColor == color) return mat;
+        
+        dfs(mat,sr,sc,color,r,c,cellColor);
+        return mat;
+    }
+};
