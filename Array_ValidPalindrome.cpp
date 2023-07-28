baf90d999f4bfd4a1b84f03d16952a504bccd666
Deep Patel <88723958+deeppatel51@users.noreply.github.com>
2023-07-28 13:19:08 +0530

    Create Array_ValidPalindrome.cpp

---
+++ Array_ValidPalindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool revStr(string& sol, int i){
+        int len=sol.length();
+        
+        if(i>=len/2) return true;
+        else if (sol[i]!=sol[len-i-1]) return false;
+        return revStr(sol, i+1);
+    }
+
+    bool isPalindrome(string s) {
+        string sol;
+
+        for(int i=0;i<s.length();i++){
+            if(s[i]>='A'&&s[i]<='Z') sol.push_back(tolower(s[i]));
+            else if(s[i]>='a'&&s[i]<='z'||s[i]>='0'&&s[i]<='9') sol.push_back(s[i]);
+        }
+        return revStr(sol, 0);
+    }
+};
