063e9df9b223ea33ef65ed012ed764997d0536b3
Deep Patel <88723958+deeppatel51@users.noreply.github.com>
2023-08-22 16:11:18 +0530

    Create Recursion_isFound.cpp

---
+++ Recursion_isFound.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+bool isSorted(int *arr, int n)
+{
+    if(n==0) return true;
+    if(arr[0]>arr[1]) return false;
+    else return isSorted(arr+1, n-1);
+}
+int main()
+{
+    int arr[5]={10,20,1000,40,50};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    if(isSorted(arr, n)) cout << "true" << endl;
+    else cout << "false" << endl;
+    return 0;
+}
