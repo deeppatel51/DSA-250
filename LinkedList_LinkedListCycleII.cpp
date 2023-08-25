7499e5a042af7f3a496a1ab147b58ac754451537
Deep Patel <88723958+deeppatel51@users.noreply.github.com>
2023-08-25 13:16:53 +0530

    Update LinkedList_LinkedListCycleII.cpp

--- LinkedList_LinkedListCycleII.cpp
+++ LinkedList_LinkedListCycleII.cpp
@@ -21,3 +21,44 @@ public:
         return NULL;
     }
 };
+
+
+/////////////// Approach 2:
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head==NULL) return NULL;
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while(fast!=NULL && slow != NULL)
+        {
+            slow = slow -> next;
+            fast = fast -> next;
+            if(fast != NULL)
+            {
+                fast = fast -> next;
+            }
+
+            if(slow==fast)
+            {
+                slow = head;
+                while(slow==fast)
+                {
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
+    }
+};
