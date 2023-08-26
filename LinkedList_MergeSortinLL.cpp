23b28e0712263a12020e59957817d172642ebb94
Deep Patel <88723958+deeppatel51@users.noreply.github.com>
2023-08-26 07:30:20 +0530

    Create LinkedList_MergeSortinLL.cpp

---
+++ LinkedList_MergeSortinLL.cpp
@@ -0,0 +1,80 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* merge2LL(ListNode* list1, ListNode* list2)
+    {
+        if(list2==NULL) return list1;
+        if(list1==NULL) return list2;
+        ListNode* ans = new ListNode(-1);
+        ListNode* temp = ans;
+        while(list1!=NULL && list2!=NULL)
+        {
+            if(list1->val < list2->val)
+            {
+                temp->next=list1;
+                temp=list1;
+                list1=list1->next;
+            }
+            else
+            {
+                temp->next=list2;
+                temp=list2;
+                list2=list2->next;
+            }
+        }
+        while(list1!=NULL)
+        {
+            temp->next=list1;
+            temp=list1;
+            list1=list1->next;
+        }
+        while(list2!=NULL)
+        {
+            temp->next=list2;
+            temp=list2;
+            list2=list2->next;
+        }
+        ans = ans->next;
+        return ans;
+    }
+    ListNode* findMid(ListNode* head) 
+    {
+        ListNode* slow = head;
+        ListNode* fast = head -> next;
+        
+        while(fast != NULL && fast -> next != NULL) 
+        {
+            slow = slow -> next;
+            fast = fast -> next -> next; 
+        }
+        return slow;
+    }
+    ListNode* mergeSort(ListNode* head) 
+    {
+        if( head == NULL || head -> next == NULL ) 
+        {
+            return head;
+        }
+        ListNode* mid = findMid(head);
+        ListNode* left = head;
+        ListNode* right = mid->next;
+        mid->next=NULL;
+        left = mergeSort(left);
+        right = mergeSort(right);
+        ListNode* result = merge2LL(left, right);
+        return result;
+    };
+    ListNode* sortList(ListNode* head)
+    {
+        return mergeSort(head);
+    }
+};    
